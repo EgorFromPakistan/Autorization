ea6c82cb635c512e739c6c204d6a7046669ed30c
Egor Gutko <egorka.gutko@gmail.com>
2019-09-18 13:58:34 +0300

    Toolbar

--- app/src/main/java/by/egorgutko/autorization/presentation/MainActivity.java
+++ app/src/main/java/by/egorgutko/autorization/presentation/MainActivity.java
@@ -1,30 +1,67 @@
 package by.egorgutko.autorization.presentation;
 
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
+
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import by.egorgutko.autorization.R;
 import data.ListFragment;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListFragment listFragment = new ListFragment();
+    //ListFragment listFragment = new ListFragment();
 
     FragmentManager fragmetManager;
+    NavController navController;
+    Button button2;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         /*setContentView(R.layout.activity_main);
         textView = findViewById(R.id.tView);
         textView.setOnClickListener(this);*/
-        setContentView(R.layout.list_activity);
+        //setContentView(R.layout.list_activity);
+        setContentView(R.layout.check);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        // button2 = (Button) findViewById(R.id.button1);
+        //button2.setOnClickListener(this);
         //fragment = new ListFragment();
-        fragmetManager = getFragmentManager();
+        // navController = Navigation.findNavController(this,R.id.placeholder);
+        /*fragmetManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmetManager.beginTransaction();
         fragmentTransaction.add(R.id.placeholder, listFragment);
-        fragmentTransaction.commit();
+        fragmentTransaction.commit();*/
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mymenu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_see:
+                navController.navigate(R.id.listFragment);
+                break;
+            case R.id.menu_add:
+                navController.navigate(R.id.forAddFragment);
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
---
+++ app/src/main/java/data/ForAddFragment.java
@@ -0,0 +1,31 @@
+package data;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import by.egorgutko.autorization.R;
+
+public class ForAddFragment extends Fragment {
+
+    TextView textView;
+    Button button;
+
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup containre, Bundle saveInstanceState) {
+
+        View view = inflater.inflate(R.layout.for_add, containre, false);
+
+
+        button =(Button) view.findViewById(R.id.button);
+        textView = (TextView)view.findViewById(R.id.etext);
+        return view;
+    }
+
+}
