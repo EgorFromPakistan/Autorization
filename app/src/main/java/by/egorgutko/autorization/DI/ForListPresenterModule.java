a99569d7db5e712a59cd07859ecb05a2419847b0
Egor Gutko <egorka.gutko@gmail.com>
2019-10-15 15:12:28 +0300

    Dagger2

---
+++ app/src/main/java/by/egorgutko/autorization/DI/ForListPresenterModule.java
@@ -0,0 +1,14 @@
+package by.egorgutko.autorization.DI;
+
+import by.egorgutko.autorization.fragments.list.ForListFragmentPresenter;
+import dagger.Module;
+import dagger.Provides;
+
+@Module
+public class ForListPresenterModule {
+
+    @Provides
+    ForListFragmentPresenter provideForListFragmentPresenter(){
+        return  new ForListFragmentPresenter();
+    }
+}
